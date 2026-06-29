3d151098a979f581b7b7a6cf20c21fef9820673b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-06-30 01:56:13 +0200

    build: chunk codebase during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules/@chakra-ui")) return "chakra-ui";
+          if (id.includes("node_modules/@zip.js/zip.js")) return "zip-js";
+          if (id.includes("node_modules/zod")) return "zod";
+          return null;
+        },
+      },
+      plugins: [
+        // visualizer({
+        //   brotliSize: true,
+        //   gzipSize: true,
+        //   open: true,
+        // }),
+      ],
+    },
+  },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
