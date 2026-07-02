7f361bdeabf42c38e8d496feeab991ba3f1c82b9
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-07-02 16:47:35 +0200

    feat: change color scheme to sea green

--- src/theme/system.ts
+++ src/theme/system.ts
@@ -30,13 +30,13 @@ export const system = createSystem(defaultConfig, {
       colors: {
         accent: {
           border: {
-            value: { _dark: "{colors.gray.800}", _light: "{colors.gray.200}" },
+            value: { _dark: "{colors.sea.700}", _light: "{colors.sea.200}" },
           },
           contrast: {
             value: { _dark: "{colors.black}", _light: "{colors.white}" },
           },
           emphasized: {
-            value: { _dark: "{colors.gray.700}", _light: "{colors.gray.300}" },
+            value: { _dark: "{colors.sea.800}", _light: "{colors.sea.100}" },
           },
           fg: {
             value: { _dark: "{colors.gray.200}", _light: "{colors.gray.800}" },
@@ -45,17 +45,52 @@ export const system = createSystem(defaultConfig, {
             value: { _dark: "{colors.gray.400}", _light: "{colors.gray.400}" },
           },
           muted: {
-            value: { _dark: "{colors.gray.800}", _light: "{colors.gray.200}" },
+            value: { _dark: "{colors.sea.900}", _light: "{colors.sea.50}" },
           },
           solid: {
             value: { _dark: "{colors.white}", _light: "{colors.gray.900}" },
           },
           subtle: {
-            value: { _dark: "{colors.gray.900}", _light: "{colors.gray.100}" },
+            value: { _dark: "{colors.sea.900}", _light: "{colors.sea.25}" },
           },
+        },
+        bg: {
+          DEFAULT: {
+            value: { _dark: "{colors.sea.950}", _light: "{colors.sea.50}" },
+          },
+          emphasized: {
+            value: { _dark: "{colors.sea.800}", _light: "{colors.sea.100}" },
+          },
+          panel: {
+            value: { _dark: "{colors.sea.900}", _light: "{colors.white}" },
+          },
+          subtle: {
+            value: { _dark: "{colors.sea.850}", _light: "{colors.sea.25}" },
+          },
+        },
+        border: {
+          value: { _dark: "{colors.sea.700}", _light: "{colors.sea.200}" },
         },
       },
     },
-    tokens: {},
+    tokens: {
+      colors: {
+        sea: {
+          "25": { value: "#f7fbfc" },
+          "50": { value: "#eef7f8" },
+          "100": { value: "#dcecef" },
+          "200": { value: "#bfd5da" },
+          "300": { value: "#93b8c0" },
+          "400": { value: "#6798a4" },
+          "500": { value: "#437a87" },
+          "600": { value: "#35636d" },
+          "700": { value: "#2e4a50" },
+          "800": { value: "#1c3337" },
+          "850": { value: "#132a2d" },
+          "900": { value: "#102225" },
+          "950": { value: "#071416" },
+        },
+      },
+    },
   },
 });
